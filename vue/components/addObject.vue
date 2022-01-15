9c569da579aadb0ab18fe90c6a89f71527e2ef12
Evbrs <ev.brs@outlook.com>
2022-01-15 20:56:47 +0100

    Visuel page Accueil + ajout d'objets

--- vue/components/addObject.vue
+++ vue/components/addObject.vue
@@ -1,35 +1,29 @@
 <template>
   <div
-    class="flex flex-col justify-center min-h-screen py-6 antialiased bg-gray-100 sm:py-9"
+    class="flex flex-col justify-center h-full min-h-full py-3 antialiased bg-gray-100 sm:py-5"
   >
     <div class="relative py-3 mx-auto text-center sm:w-96">
       <span class="text-xl font-light">Ajouter un objet</span>
       <div class="mt-4 text-left bg-gray-200 rounded-lg bg-white-shadow">
-        <div class="h-2 bg-indigo-400 rounded-t-md"></div>
+        <div class="h-2 bg-blue-400 rounded-t-md"></div>
         <div class="px-8 py-6">
           <label class="block font-semibold">Type d'objet</label>
           <input
             type="text"
             placeholder="Type de l'objet"
-            class="w-full h-5 px-3 py-5 mt-2 border rounded-md hover:outline-none focus:outline-none focus:ring-1 focus:ring-indigo-500"
+            class="w-full h-5 px-3 py-5 mt-2 border rounded-md hover:outline-none focus:outline-none focus:ring-1 focus:ring-blue-500"
           />
           <label class="block mt-3 font-semibold"
             >Lieu où vous l'avez trouvé</label
           >
           <input
-            type="email"
-            placeholder="Email"
-            class="w-full h-5 px-3 py-5 mt-2 border rounded-md hover:outline-none focus:outline-none focus:ring-1 focus:ring-indigo-500"
-          />
-          <label class="block mt-3 font-semibold">Mot de passe</label>
-          <input
-            type="password"
-            placeholder="Password"
-            class="w-full h-5 px-3 py-5 mt-2 border rounded-md hover:outline-none focus:outline-none focus:ring-1 focus:ring-indigo-500"
+            type="text"
+            placeholder="Lieu"
+            class="w-full h-5 px-3 py-5 mt-2 border rounded-md hover:outline-none focus:outline-none focus:ring-1 focus:ring-blue-500"
           />
           <div class="flex items-baseline justify-between">
             <button
-              class="px-6 py-2 mt-4 text-white bg-indigo-500 rounded-md hover:bg-indigo-600"
+              class="px-6 py-2 mt-4 text-white bg-blue-500 rounded-md hover:bg-blue-600"
             >
               Valider
             </button>
--- vue/pages/index.vue
+++ vue/pages/index.vue
@@ -1,12 +1,28 @@
 <template>
-  <NuxtLayout name="custom">
-    <div></div>
+  <NuxtLayout name="custom" class="lg:h-screen lg:pb-16">
+    <div
+      class="flex flex-col justify-center text-lg text-center bg-gray-100 h-36 sm:py-9"
+    >
+      <div
+        class="flex flex-col justify-center w-3/6 mx-auto border-2 border-gray-300 h-36 bg-gray-200/90 rounded-3xl"
+      >
+        <p class="text-xl font-semibold text-blue-500">
+          Bienvenue sur le test technique de Troov.com
+        </p>
+        <p>Si vous avez trouvé un objet vous pouvez l'ajouter ci-dessous</p>
+      </div>
+    </div>
+    <add-object />
+    <div class="bg-gray-100 h-28"></div>
   </NuxtLayout>
 </template>
 
 <script>
 import { defineComponent } from "vue";
-export default defineComponent({});
+import addObject from "../components/addObject.vue";
+export default defineComponent({
+  components: { addObject },
+});
 </script>
 
 <style></style>
