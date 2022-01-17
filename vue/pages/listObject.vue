2e789d5fd46cec373ef5f82e1e1dcc31f7904995
Evbrs <ev.brs@outlook.com>
2022-01-17 16:28:42 +0100

    Style Liste des objets

--- vue/pages/listObject.vue
+++ vue/pages/listObject.vue
@@ -1,15 +1,21 @@
 <template>
   <NuxtLayout name="custom">
     <div>
-      <h1 class="">Event Listing</h1>
-      <ul
-        v-for="event in events"
-        :key="event.id"
-        :event="event"
-        class="flex flex-col justify-center w-1/2 mx-auto mb-8 text-center border-2 border-blue-300 rounded-xl"
+      <h1
+        class="flex flex-col justify-center w-full py-10 text-5xl text-center"
       >
-        <li>{{ event.objectDescription }}</li>
-        <li>{{ event.place }}</li>
+        Event Listing
+      </h1>
+      <ul
+        class="grid w-2/3 grid-flow-row grid-cols-3 mx-auto gap-x-3 gap-y-2 auto-rows-fr"
+      >
+        <template v-for="event in events" :key="event.id">
+          <li
+            class="block mb-5 font-medium text-center text-white bg-blue-300 border-2 border-blue-500 rounded-xl"
+          >
+            {{ event.objectDescription + " - " + event.place }}
+          </li>
+        </template>
       </ul>
     </div>
   </NuxtLayout>
